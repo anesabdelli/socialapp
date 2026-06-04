3f1b3d3f2f85e4246555382c841a53c347247c86
Lilian <lilian@MacBook-Air-de-Lilian.local>
2026-06-04 10:40:54 +0200

    commentaire

--- src/main/java/com/example/socialapp/model/FriendRequest.java
+++ src/main/java/com/example/socialapp/model/FriendRequest.java
@@ -1,23 +1,23 @@
 package com.example.socialapp.model;
 
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.EnumType;
-//import jakarta.persistence.Enumerated;
-//import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-//@Entity
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 public class FriendRequest {
-//    @Id
+    @Id
     private String id;
     private String senderId;
     private String receiverId;
-//    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.STRING)
     private FriendRequestStatus status;
 
     public FriendRequest(String id, String senderId, String receiverId, FriendRequestStatus status) {
